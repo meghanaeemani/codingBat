d354f4bf0635c43b58e6a3fd25b25a663973201e
meghana <meghanaeemani@gmail.com>
2026-03-05 17:49:18 -0500

    all 5 problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,12 @@ public class Main {
         Warmup1 w = new Warmup1();
         boolean result = w.sleepIn(true,false);
         System.out.println(result);
+        boolean result2 =w.monkeyTrouble(true, true);
+        System.out.println(result2);
+        System.out.println(w.sumDouble(3,3));
+        System.out.println(w.diff21(25));
+        System.out.println(w.parrotTrouble(true, 6));
+
     }
 
 }
--- src/main/java/org/example/Warmup1.java
+++ src/main/java/org/example/Warmup1.java
@@ -7,5 +7,31 @@ public class Warmup1 {
         }
         return false;
         }
-    }
+    //We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    // We are in trouble if they are both smiling or if neither of them is smiling.
+    // Return true if we are in trouble.
+        public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+            return (aSmile && bSmile) || (!aSmile && !bSmile);
+        }
+        public int sumDouble(int a, int b) {
+            if (a == b) {
+                return 2 * (a + b);
+            }
+            return a + b;
+        }
+        public int diff21(int n) {
+            if (n > 21) {
+                return 2 * (n - 21);
+            }
+            return 21 - n;
+        }
+        public boolean parrotTrouble(boolean talking, int hour){
+        if (!talking)
+            return false;
+            return (hour < 7 || hour > 20);
+        }
+        }
+
+
+
 
