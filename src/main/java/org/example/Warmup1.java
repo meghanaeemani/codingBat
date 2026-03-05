d131401e1d8a9252affa054827474c291df6db64
meghana <meghanaeemani@gmail.com>
2026-03-05 18:31:28 -0500

    add questions to all 5 problem

--- src/main/java/org/example/Warmup1.java
+++ src/main/java/org/example/Warmup1.java
@@ -13,18 +13,36 @@ public class Warmup1 {
         public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
             return (aSmile && bSmile) || (!aSmile && !bSmile);
         }
+        //Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+        //
+        //
+        //sumDouble(1, 2) → 3
+        //sumDouble(3, 2) → 5
+        //sumDouble(2, 2) → 8
         public int sumDouble(int a, int b) {
             if (a == b) {
                 return 2 * (a + b);
             }
             return a + b;
         }
+        //Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+        //
+        //
+        //diff21(19) → 2
+        //diff21(10) → 11
+        //diff21(21) → 0
         public int diff21(int n) {
             if (n > 21) {
                 return 2 * (n - 21);
             }
             return 21 - n;
         }
+        //We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+        //
+        //
+        //parrotTrouble(true, 6) → true
+        //parrotTrouble(true, 7) → false
+        //parrotTrouble(false, 6) → false
         public boolean parrotTrouble(boolean talking, int hour){
         if (!talking)
             return false;
